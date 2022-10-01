b7af5f11a99ff33dc498a48c2c8414404244949e
0b51d14n <arnav21703@gmail.com>
2022-10-01 19:11:19 +0530

    React webpage with css

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,18 @@
+import "./home.css"
+import aasmaan from "./imgs/aasmaan.svg"
+import blob from "./imgs/blob.svg"
+import aasmaanuncle1 from "./imgs/aasmaanuncle1.png"
+import aasmaanuncle2 from "./imgs/aasmaanuncle2.png"
+import footer1 from "./imgs/footer1.jpg"
+import footer2 from "./imgs/footer2.jpg"
+import footer3 from "./imgs/footer3.jpg"
+import logo from "./imgs/logo.png"
+
 export default function Home(){
     return(<>
         <nav>
     <div className="aasmaancontainer">
-        <img src="./aasmaan.svg"></img>
+        <img src={aasmaan}></img>
     </div>
         <ul>
             <li><a href="#home">Home</a></li>
@@ -14,27 +24,27 @@ export default function Home(){
     </nav>
     <div className="mid">
         <div className="edutext">Education is not received, it is achieved.</div>
-        <img className="blob" src="./blob.svg"></img>
+        <img className="blob" src={blob}></img>
     </div>
     <button className="volunteer">Volunteer Today</button>
     <div className="four">
-        <img className="img1" src="./aasmaanuncle1.png"></img>
+        <img className="img1" src={aasmaanuncle1}></img>
         <p className="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p className="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <img className="img2" src="./aasmaanuncle2.png"></img>
+        <img className="img2" src={aasmaanuncle2}></img>
     </div>
     <footer>
         <div>
-            <img classNameName="logo" src="./logo.png"></img>
-            <p style="font-family: 'Raleway',sans-serif; font-size: 1rem; color: white;">
+            <img className="logo" src={logo}></img>
+            <p>
                 ©️ Copyright 2022 - <br />
                 Chicanery @ HackOverflow<br />
                 All Rights Reserved.
             </p>
         </div>
-        <img src="./footer1.jpg" alt="footer1"></img>
-        <img src="./footer2.jpg" alt="footer2"></img>
-        <img src="./footer3.jpg" alt="footer3"></img>
+        <img src={footer1} alt="footer1"></img>
+        <img src={footer2} alt="footer2"></img>
+        <img src={footer3} alt="footer3"></img>
     </footer>
     </>);
 }
