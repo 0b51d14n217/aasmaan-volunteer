f50f63916efa3d3117bf97d5125464e384c8edc4
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-10-01 20:08:45 +0530

    dashbaord

--- src/routes/signedin.js
+++ src/routes/signedin.js
@@ -1,5 +1,4 @@
 import firebaseApp from "../firebase/client";
-import { collection, getFirestore } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 
@@ -11,15 +10,30 @@ export default function SignedIn() {
   //  snapshotListenOptions: { includeMetadataChanges: true },
   //});
   //const [tasks, taskloading, taskerror] = useCollection(collection(getFirestore(firebaseApp), "tasks"), {});
-
-  const [tasks, taskloading, taskerror] = useCollection(firebaseApp.firestore().collection("votes"), {
+  const db = firebaseApp.firestore();
+  const [tasks, taskloading, taskerror] = useCollection(firebaseApp.firestore().collection("tasks"), {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
   console.log(tasks, "|", taskloading, "|", taskerror);
-
   return (
     <>
       <h1>Hello !</h1>
+      {tasks?.docs?.map((doc) => {
+        const Accept = async () => {
+          const id = doc.id;
+          const userid = user.id;
+          await db.collection("tasks").doc(db, "tasks", id).update({ status: "accepted", user: userid });
+        };
+        return (
+          <div key={doc.id}>
+            {console.log(doc.data())}
+            <p>{doc.data().videolink}</p>
+            <p>{doc.data().phone}</p>
+            <button onClick={Accept(doc.data(), doc.id, user.uid)}>Accept</button>
+            {/*<p>{doc.data().location}</p>*/}
+          </div>
+        );
+      })}
     </>
   );
 }
